95cfbd04b977b72517498a1b189d7f567f0b62eb
_Yury_ <90544915+c0de1sl1fe@users.noreply.github.com>
2022-11-13 20:01:55 +0400

    add protecteded fiature

--- main.py
+++ main.py
@@ -17,14 +17,14 @@ class Example(QWidget):
     def __init__(self) -> None:
         '''Constructor'''
         super().__init__()
-        self.mainBtn = QPushButton("Choose dataset dir", self)
-        self.mainBtn.clicked.connect(self.__inputPath)
-        self.iterator = Iterator1_img("test", "dataset")
+        self.__mainBtn = QPushButton("Choose dataset dir", self)
+        self.__mainBtn.clicked.connect(self.__inputPath)
+        self.__iterator = Iterator1_img("test", "dataset")
         # self.pixmap = QPixmap('blueLobster.jpg')
-        self.pixmap = QPixmap('.jpg')
-        self.lable = QLabel(self)
-        self.name = "test"
-        self.path = "dataset"
+        self.__pixmap = QPixmap('.jpg')
+        self.__lable = QLabel(self)
+        self.__name = "test"
+        self.__path = "dataset"
         self.initUI()
 
     # def __iter__(self) -> Self:
@@ -39,10 +39,10 @@ class Example(QWidget):
         layout = QVBoxLayout()
         self.setLayout(layout)
         tabs = QTabWidget()
-        tabs.addTab(self.generalTab(), "general")
-        tabs.addTab(self.showImageTab(), "show image")
-        tabs.addTab(self.tasksTab(), "tasksTab")
-        self.iterator.setPath(self.path)
+        tabs.addTab(self.__generalTab(), "general")
+        tabs.addTab(self.__showImageTab(), "show image")
+        tabs.addTab(self.__tasksTab(), "tasksTab")
+        self.__iterator.setPath(self.__path)
         layout.addWidget(tabs)
 
         self.setGeometry(300, 300, 350, 300)
@@ -53,7 +53,7 @@ class Example(QWidget):
         self.show()
 
     # Main window with hello-words and descripition what's going on
-    def generalTab(self) -> QWidget:
+    def __generalTab(self) -> QWidget:
         '''main window with hello-words and descripition what's going on'''
         custom_font = QFont()
         custom_font.setPixelSize(40);        
@@ -70,52 +70,52 @@ class Example(QWidget):
         text.addWidget(line1)
         text.addWidget(line2)
         layout.addLayout(text)
-        layout.addWidget(self.mainBtn)
+        layout.addWidget(self.__mainBtn)
 
         generalTab.setLayout(layout)
         return generalTab
 
     # image tab where user choose directory and see the img via _next_
-    def showImageTab(self) -> QWidget:
+    def __showImageTab(self) -> QWidget:
         '''image tab where user choose directory and see the img via _next_'''
         generalTab = QWidget()
         layout = QVBoxLayout()
 
         layoutButton = QHBoxLayout()
         clearBtn = QPushButton("clear")
-        clearBtn.clicked.connect(self.clearButton)
+        clearBtn.clicked.connect(self.__clearButton)
         nextBtn = QPushButton("next")
-        nextBtn.clicked.connect(self.nextButton)
+        nextBtn.clicked.connect(self.__nextButton)
         layoutButton.addWidget(clearBtn)
         layoutButton.addWidget(nextBtn)
 
-        self.lable.setPixmap(self.pixmap)
-        layout.addWidget(self.lable)
-        self.resize(self.pixmap.width(), self.pixmap.height())
+        self.__lable.setPixmap(self.__pixmap)
+        layout.addWidget(self.__lable)
+        self.resize(self.__pixmap.width(), self.__pixmap.height())
 
         # layout.addLayout(layoutRadioButton)
         layout.addLayout(layoutButton)
         generalTab.setLayout(layout)
         return generalTab
 
-    def tasksTab(self) -> QWidget:
+    def __tasksTab(self) -> QWidget:
         '''function generates tab with all buttons of prev lab'''
         generalTab = QWidget()
         layout = QVBoxLayout()
         
         layout = QVBoxLayout()
         task1 = QPushButton('task1')
-        task1.clicked.connect(self.task1)
+        task1.clicked.connect(self.__task1)
         layout.addWidget(task1)
 
         task2 = QPushButton('task2')
-        task2.clicked.connect(self.task2)
+        task2.clicked.connect(self.__task2)
         layout.addWidget(task2)
 
         task3Single = QPushButton('task3Single')
-        task3Single.clicked.connect(self.task3Single)
+        task3Single.clicked.connect(self.__task3Single)
         task3Both = QPushButton('task3Both')
-        task3Both.clicked.connect(self.task3Both)
+        task3Both.clicked.connect(self.__task3Both)
         layout.addWidget(task3Single)
         layout.addWidget(task3Both) 
         layout.addSpacing(100)
@@ -123,18 +123,18 @@ class Example(QWidget):
         layoutRadioButton = QHBoxLayout()
         radiobutton = QRadioButton("zebra")
         radiobutton.name = "zebra"
-        radiobutton.toggled.connect(self.onClicked)
+        radiobutton.toggled.connect(self.__onClicked)
         layoutRadioButton.addWidget(radiobutton)
 
         radiobutton = QRadioButton("bay horse")
         radiobutton.name = "bay horse"
-        radiobutton.toggled.connect(self.onClicked)
+        radiobutton.toggled.connect(self.__onClicked)
         layoutRadioButton.addWidget(radiobutton)
         radiobutton = QRadioButton("test")
 
         radiobutton.name = "test"
         radiobutton.setChecked(True)
-        radiobutton.toggled.connect(self.onClicked)
+        radiobutton.toggled.connect(self.__onClicked)
 
         layoutRadioButton.addWidget(radiobutton)
         layout.addLayout(layoutRadioButton)
@@ -142,13 +142,13 @@ class Example(QWidget):
         generalTab.setLayout(layout)
         return generalTab
 
-    def onClicked(self) -> None:
+    def __onClicked(self) -> None:
         '''change class name'''
         radioButton = self.sender()
         if radioButton.isChecked():
             print("Class is %s" % (radioButton.name))
-            self.iterator.setName(radioButton.name)
-            self.name = radioButton.name
+            self.__iterator.setName(radioButton.name)
+            self.__name = radioButton.name
 
     def __inputPath(self) -> None:
         '''service function'''
@@ -159,59 +159,59 @@ class Example(QWidget):
             QMessageBox.critical(
                 self, "Wrong dir", "please choose dir with dataset", QMessageBox.Ok)
         else:
-            print("prev dir is ", self.path)
-            self.path = tmp 
-            print("new dir is ", self.path)
+            print("prev dir is ", self.__path)
+            self.__path = tmp 
+            print("new dir is ", self.__path)
 
-    def clearButton(self) -> None:
+    def __clearButton(self) -> None:
         '''toggle clear and start iteration again'''
         print("clear")
-        self.pixmap = QPixmap(".jpg")
-        self.lable.setPixmap(self.pixmap)
-        self.iterator.clear()
+        self.__pixmap = QPixmap(".jpg")
+        self.__lable.setPixmap(self.__pixmap)
+        self.__iterator.clear()
         self.resize(300, 300)
 
-    def nextButton(self) -> None:
+    def __nextButton(self) -> None:
         '''toggle next and operate with exception'''
         try:
-            tmp = os.path.join(os.path.join(self.iterator.path,
-                                            self.iterator.name), self.iterator.__next__())
-            self.pixmap = QPixmap(
+            tmp = os.path.join(os.path.join(self.__iterator.path,
+                                            self.__iterator.name), self.__iterator.__next__())
+            self.__pixmap = QPixmap(
                 f"{tmp}")
-            self.lable.setPixmap(self.pixmap)
+            self.__lable.setPixmap(self.__pixmap)
             print(tmp)
         except:
-            self.pixmap = QPixmap('blueLobster.jpg')
-            self.lable.setPixmap(self.pixmap)
+            self.__pixmap = QPixmap('blueLobster.jpg')
+            self.__lable.setPixmap(self.__pixmap)
             reply = QMessageBox.question(self, 'End of img_class',
                                          "empry, clear?", QMessageBox.Yes |
                                          QMessageBox.No, QMessageBox.Yes)
             if reply == QMessageBox.Yes:
-                self.iterator.clear()
+                self.__iterator.clear()
             print("Error")
 
-    def task1(self) -> None:
+    def __task1(self) -> None:
         '''function for task1'''
         print("done task1!")
-        create_csv(self.name, self.path)
+        create_csv(self.__name, self.__path)
 
-    def task2(self) -> None:
+    def __task2(self) -> None:
         '''function for task2'''
         print("done task2!")
-        copy_dataset(self.name, self.path, QFileDialog.getExistingDirectory(
+        copy_dataset(self.__name, self.__path, QFileDialog.getExistingDirectory(
             self, 'Select Folder'))
 
-    def task3Single(self) -> None:
+    def __task3Single(self) -> None:
         '''function for task3 for single class'''
         print("done task3 single!")
         create_dir_copy_randNames(
-            self.name, self.path, QFileDialog.getExistingDirectory(self, 'Select Folder'))
+            self.__name, self.__path, QFileDialog.getExistingDirectory(self, 'Select Folder'))
 
-    def task3Both(self) -> None:
+    def __task3Both(self) -> None:
         '''function for task3 for both classes'''
         print("done task3 both!")
         create_dir_copy_randNames_both(
-            'zebra', 'bay horse', self.path, QFileDialog.getExistingDirectory(self, 'Select Folder'))
+            'zebra', 'bay horse', self.__path, QFileDialog.getExistingDirectory(self, 'Select Folder'))
 
 
 if __name__ == '__main__':
